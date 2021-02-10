6d138c96ef873cb795c53aa21e4ea6f67f6a72c2
ujjayiniP8 <77386374+ujjayiniP8@users.noreply.github.com>
2021-02-10 18:47:16 +0530

    Add files via upload

---
+++ CrumpledBalls.js
@@ -0,0 +1,11 @@
+class CrumbledBalls extends Paper{
+  constructor(x,y){
+    super(x,y,50,50);
+    this.image = loadImage("paper.png");
+  }
+  display(){
+    this.body.position.x = mouseX;
+    this.body.position.y = mouseY;
+    super.display();
+  }
+}
